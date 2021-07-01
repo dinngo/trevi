d0d3fe4f221adba56b45dcf6f2ff33bed843ab29
benhuang <ben@dinngo.co>
2021-07-01 13:42:18 +0800

    Add condition check

--- contracts/examples/FlashBorrower.sol
+++ contracts/examples/FlashBorrower.sol
@@ -13,6 +13,7 @@ contract FlashBorrower is IERC3156FlashBorrower {
         uint256 fee,
         bytes calldata data
     ) external override returns (bytes32) {
+        require(IERC20(token).balanceOf(address(this)) >= amount, "No borrow funds");
         initiator;
         data;
         IERC20(token).approve(msg.sender, amount+fee);
@@ -24,8 +25,7 @@ contract FlashBorrower is IERC3156FlashBorrower {
         IERC20 token,
         uint256 amount,
         uint256 multiplier
-    ) external returns (bool)
-{
+    ) external returns (bool) {
         token.transferFrom(msg.sender, address(this), amount);
         bytes memory data = new bytes(0);
         return flash.flashLoan(this, address(token), amount*multiplier, data);
