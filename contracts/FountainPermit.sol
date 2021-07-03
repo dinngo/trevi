fa089779c1340e9fdda3b53a08ee28a74ef4148b
benhuang <ben@dinngo.co>
2021-07-03 21:43:03 +0800

    Fix comments

--- contracts/FountainPermit.sol
+++ contracts/FountainPermit.sol
@@ -4,16 +4,6 @@ pragma solidity 0.6.12;
 
 import "./FountainBase.sol";
 
-/**
- * @dev  Implementation of the ERC20 Permit extension allowing approvals to be made via signatures, as defined in
- * https://eips.ethereum.org/EIPS/eip-2612[EIP-2612].
- *
- * Adds the {permit} method, which can be used to change an account's ERC20 allowance (see {IERC20-allowance}) by
- * presenting a message signed by the account. By not relying on `{IERC20-approve}`, the token holder account doesn't
- * need to send a transaction, and thus is not required to hold Ether at all.
- *
- * _Available since v3.4._
- */
 abstract contract FountainPermit is FountainBase {
     using Counters for Counters.Counter;
 
@@ -27,8 +17,8 @@ abstract contract FountainPermit is FountainBase {
         );
 
     /**
-     *  @dev Emitted when the allowance of a `sender` for an `owner` is set by
-     * a call to {approve}. `timeLimit` is the new allowance.
+     *  @dev Emitted when the time limit of a `sender` for an `owner` is set by
+     * a call to {approve}. `timeLimit` is the new time limit.
      */
     event HarvestApproval(
         address indexed owner,
@@ -158,7 +148,7 @@ abstract contract FountainPermit is FountainBase {
     }
 
     /**
-     * @dev Sets `timeLimit` as the time allowance of `sender` over the
+     * @dev Sets `timeLimit` as the time limit of `sender` over the
      * `owner` s tokens.
      *
      * This internal function is equivalent to `approve`, and can be used to
