46e5bb658a08e1c3013f4adf195545ff3cdd8400
Robin Pan <robin@dinngo.co>
2021-12-13 14:35:16 +0800

    Config contract linking for lendingToken of angel

--- specs/scripts/angel.sh
+++ specs/scripts/angel.sh
@@ -7,6 +7,6 @@ fi
 B=2
 certoraRun contracts/Fountain.sol contracts/Angel.sol specs/harnesses/DummyERC20A.sol specs/harnesses/DummyERC20B.sol specs/harnesses/Summary.sol \
     --verify Angel:specs/angel.spec \
-    --link Fountain:stakingToken=DummyERC20A Fountain:lendingToken=DummyERC20A Angel:GRACE=DummyERC20B \
+    --link Fountain:stakingToken=DummyERC20A Fountain:lendingToken=DummyERC20A Angel:GRACE=DummyERC20B Angel:lendingToken=DummyERC20B \
     --settings -assumeUnwindCond,-t=600,-ignoreViewFunctions,-b=$B \
     --msg "Angel - $1"
